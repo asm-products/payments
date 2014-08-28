490db2bb150164edc3210c04df8490fd24f39fe1
Charles Pletcher <cpletcher2@gmail.com>
2014-08-27 21:40:43 -0700

    CRUD methods for plans Create Stripe plans, storing and passing only the essentials on to the end user

--- app/server/controllers/plans_controller.js
+++ app/server/controllers/plans_controller.js
@@ -1,34 +1,109 @@
+var crypto = require('crypto');
 var request = require('co-request');
 var thunk = require('thunkify');
 var render = require('../lib/render');
+var stripe = require('stripe')(process.env.STRIPE_SECRET);
 var Plan = require('../models/plan');
 
 module.exports = {
+  index: function *(next) {
+    try {
+      var plans = yield getPlans(this.params.product);
+      var ret = [];
+
+      for (var i = 0, l = plans.length; i < l; i++) {
+        var plan = plans[i];
+
+        ret.push({
+          stripe_id: plan.stripe_id,
+          product_id: plan.product_id
+        });
+      }
+
+      this.body = ret;
+    } catch (e) {
+      handleError.call(this, e);
+    }
+  },
+
   create: function *(next) {
     this.accepts('application/json');
 
-    var plan = new Plan(this.request.body);
+    var body = this.request.body;
 
-    plan.save = thunk(plan.save);
+    body.id = crypto.randomBytes(10).toString('base64');
+    body.currency = body.currency || 'usd';
 
     try {
-      this.body = yield plan.save();
-    } catch (e) {
-      console.error(e);
-      this.body = e;
-    }
+      var stripePlan = yield createStripePlan(body);
+      var plan = yield savePlan(stripePlan, this.params.product);
 
+      this.body = { stripe_plan_id: plan.stripe_id };
+    } catch (e) {
+      handleError.call(this, e);
+    }
   },
 
   show: function *(next) {
-
+    try {
+      this.body = yield retrieveStripePlan(this.params.plan);
+    } catch (e) {
+      handleError.call(this, e);
+    }
   },
 
-  update: function *(next) {
+  /**
+   * @method update()
+   * @body { name: String }
+   * Stripe: "Other plan details (price, interval, etc.) are, by design, not editable."
+   */
 
+  update: function *(next) {
+    try {
+      this.body = yield updateStripePlan(this.params.plan, this.request.body);
+    } catch (e) {
+      handleError.call(this, e);
+    }
   },
 
   destroy: function *(next) {
-
+    try {
+      this.body = yield deleteStripePlan(this.params.plan);
+    } catch (e) {
+      handleError.call(this, e);
+    }
   }
 };
+
+function *retrieveStripePlan(id) {
+  return yield stripe.plans.retrieve(id);
+}
+
+function *createStripePlan(body) {
+  return yield stripe.plans.create(body);
+}
+
+function *updateStripePlan(id, body) {
+  return yield stripe.plans.update(id, body);
+}
+
+function *deleteStripePlan(id) {
+  return yield stripe.plans.del(id);
+}
+
+function *getPlans(product) {
+  return yield Plan.find({ product_id: product }).exec();
+}
+
+function *savePlan(stripePlan, product) {
+  return yield Plan.create({
+    name: stripePlan.name,
+    product_id: product,
+    stripe_id: stripePlan.id
+  });
+}
+
+function handleError(e) {
+  this.status = e.status || 500;
+  this.body = e.message;
+}
--- app/server/models/plan.js
+++ app/server/models/plan.js
@@ -1,40 +1,27 @@
 var mongoose = require('mongoose');
+var request = require('co-request');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
 var PlanSchema = new Schema({
-  // required by Stripe
-  amount: {
-    type: Number,
-    required: true
-  },
-
-  currency: {
-    type: String,
-    default: 'usd'
-  },
-
-  name: {
-    type: String,
-    required: true,
-    index: true,
-    unique: true
-  },
-
-  // optional for stripe
-  interval_count: Number,
-  trial_period_days: Number,
-  metadata: Object,
-  statement_description: String,
-
-  // required for payments
-  product_id: {
-    type: String,
-    index: true
-  },
   created_at: {
     type: Date,
     default: Date.now
+  },
+
+  name: String,
+
+  product_id: {
+    type: String,
+    index: true,
+    required: true
+  },
+
+  stripe_id: {
+    type: String,
+    index: true,
+    required: true,
+    unique: true
   }
 });
 
