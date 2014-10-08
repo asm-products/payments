b014e1d23b29c2e894fa0ec8bb52faf3bd82f21c
Charles Pletcher <cpletcher2@gmail.com>
2014-10-08 13:12:17 -0700

    Don't export the setup function in reset.js

--- test/reset.js
+++ test/reset.js
@@ -1,17 +1,17 @@
 var mongoose = require('mongoose');
 
-exports.setup = function(done) {
+exports.dropDatabase = function(done) {
+  setup(function() {
+    mongoose.connection.db.dropDatabase(done)
+  });
+};
+
+function setup(done) {
   if (mongoose.connection.db._state === 'connected') {
     return done();
   }
 
   mongoose.connection.on('connected', function() {
     mongoose.connection.db.dropDatabase(done);
-  });
-};
-
-exports.dropDatabase = function(done) {
-  exports.setup(function() {
-    mongoose.connection.db.dropDatabase(done)
   });
 };
