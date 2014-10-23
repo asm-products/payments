b64d7e28e161a678dda34f3fc388dc3f3316cb35
Charles Pletcher <cpletcher2@gmail.com>
2014-10-23 15:32:51 -0700

    Fix "connected to undefined" logging

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
+var dbUrl = process.env.MONGOHQ_URL || '127.0.0.1';
 
-module.exports = mongoose.connect(process.env.MONGOHQ_URL || '127.0.0.1');
+module.exports = mongoose.connect(dbUrl);
 
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-console.log('connected to', process.env.MONGOHQ_URL);
+console.log('connected to', dbUrl);
